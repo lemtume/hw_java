e6de5223c5dc7891fcb082da8de7098b01588d4b
lemtume <lemtugov@gmail.com>
2019-02-01 02:19:48 +0300

    0.0.9 //серверная часть и клиентская часть - c тестами, но в 1 проекте
    0.0.8
    // services через enum с привязкой стоимости
    
    0.0.7
    //subscriber.ser без пути
    
    0.0.5
    
    //перенес файл subscriber.ser в папку с проектом:
    //при пустом файле надо менять код для входа, нужно расставить комменты;
    
    0.0.4
    //сериализация - убрал ошибку сериализации при использовании интерфейса:
    //private static final long serialVersionUID=-5250997439565143459L;
    
    // подготовил основу для объединения subscriber и services через arrayList

--- src/client/SubscriberTest.java
+++ src/client/SubscriberTest.java
@@ -25,9 +25,50 @@ public class SubscriberTest {
             for (Subscriber e : mySubscriber) {
                 System.out.println("Subscriber" + e);
             }
+
+
+            int choice;
+            int ignore;
+
+            // ArrayList<Services> ServicesList = new ArrayList<Services> ();
+
             Menu menuObj = new Menu ();
             //menuObj.readFile ();
             menuObj.LoginPassword ();
+
+            for (int LoginAttemp = 2; LoginAttemp >= 0; LoginAttemp--) {         // цикл по проверке количества неудачных входов
+                label:
+                do {
+
+                    if (menuObj.LoginInput () == true) {                    // если вход произведён
+
+                        for (; ; ) {
+
+                            do {
+                                menuObj.showMenu ();
+
+                                choice = (char) System.in.read ();
+
+                                do {
+                                    ignore = (char) System.in.read (); // игнор ввода
+
+                                } while (ignore != '\n');
+                            }
+                            while (menuObj.isValid ( choice ));//while (!choice.hasNextInt()); //
+                            if (choice == 'q') {
+                                LoginAttemp = 0;
+                                break /*label*/;
+                            }
+
+                            System.out.println ( "\n" );
+                            menuObj.helpOn ( choice );
+                        }
+                    }
+                    System.out.println ( "Осталось попыток: " + LoginAttemp + "\n\n" );
+                    break label;
+                }
+                while (menuObj.LoginInput () != true);
+            }
 
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(SubscriberTest.class.getName()).log( Level.SEVERE, null, ex);
